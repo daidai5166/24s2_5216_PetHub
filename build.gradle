b92321d40ff22c587a3cad02741ea6f4ad331112
ZhiyuanLiu <liuzhiyuqn1326@163.com>
2024-09-30 15:54:45 +1000

    添加了Google Maps的API

--- build.gradle
+++ build.gradle
@@ -4,4 +4,5 @@ plugins {
 
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
 }
