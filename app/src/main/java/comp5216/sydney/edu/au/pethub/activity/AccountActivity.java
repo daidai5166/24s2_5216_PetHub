f983e1a684fb1170be844f2ca14afcf12090ac2a
Chalrie <lzha0454@uni.sydney.edu.au>
2024-10-07 18:31:43 +1100

    Charlie改了一些页面跳转的文件比如 xml 和.java文件，正在继续写代码。王总要改轮播图，所以我先 push一下试试

--- app/src/main/java/comp5216/sydney/edu/au/pethub/activity/AccountActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/pethub/activity/AccountActivity.java
@@ -7,11 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
 
 import comp5216.sydney.edu.au.pethub.R;
 
-public class AccountActivity extends AppCompatActivity {
 
+public class AccountActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,15 @@ public class AccountActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        // 设置 account界面 Edit Profile 的点击事件监听器
+        findViewById(R.id.tv_edit_profile).setOnClickListener(v -> {
+            Intent intent = new Intent(AccountActivity.this, EditProfileActivity.class);
+            startActivity(intent);
+        });
+        // 设置 account界面 My Pet 的点击事件监听器
+        findViewById(R.id.tv_my_pet).setOnClickListener(v -> {
+            Intent intent = new Intent(AccountActivity.this, MyPetsActivity.class);
+            startActivity(intent);
+        });
     }
 }
