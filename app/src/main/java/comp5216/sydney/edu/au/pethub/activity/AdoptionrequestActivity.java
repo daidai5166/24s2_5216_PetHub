45324cdc27cecb462c212d398ba36e86e9e85a32
Chalrie <lzha0454@uni.sydney.edu.au>
2024-10-12 21:37:10 +1100

    Charlie 把所有的 xml 对应的 activity 创建。

---
+++ app/src/main/java/comp5216/sydney/edu/au/pethub/activity/AdoptionrequestActivity.java
@@ -0,0 +1,31 @@
+package comp5216.sydney.edu.au.pethub.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import comp5216.sydney.edu.au.pethub.R;
+
+public class AdoptionrequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_adoptionrequest);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // 初始化导航栏
+        NavigationBarActivity navigationBarActivity = new NavigationBarActivity(this);
+        navigationBarActivity.setupNavigationBar();
+
+    }
+}
